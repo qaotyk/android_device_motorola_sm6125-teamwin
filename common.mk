a25d177df4a617c102e893dccae98481c5acb0bf
qaotyk <mayniakgd@gmail.com>
2026-06-05 19:32:42 -0600

    sm6125-common: Add inherit 64-bit core configs and install GSI keys for verified boot support
    Signed-off-by: qaotyk <mayniakgd@gmail.com>

--- common.mk
+++ common.mk
@@ -16,6 +16,10 @@
 
 # Inherit AOSP common base configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Install gsi keys into ramdisk, to boot a GSI with verified boot
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # A/B updater
 AB_OTA_UPDATER := true
